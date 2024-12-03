0539592b38f1711c2f5daf3ca55622c72ff20023
g-smartynov <s.martynov@geniusee.com>
2024-12-03 13:03:42 +0200

    fix max header width

--- app/_ui/Sections/Header.js
+++ app/_ui/Sections/Header.js
@@ -4,14 +4,16 @@ import Logo from "../SectionElements/Logo";
 import NavLink from "../SectionElements/NavLink";
 
 export default function Header() {
-    return <nav id="header" className="fixed w-full bg-white top-0 flex justify-between items-center px-4 footerPhoneBreak:px-10 py-4 text-xl z-50">
-        <NavLink>
-            <Logo />
-        </NavLink>
-        <Link href="https://moth-app-prod-qoyzv.ondigitalocean.app/" target="_blank" rel="noopener noreferrer">
-            <KoshaButton type="secondary">
-                Join Now
-            </KoshaButton>
-        </Link>
+    return <nav id="header" className="fixed w-full bg-white top-0 flex justify-center z-50">
+        <div className="flex justify-between items-center px-4 footerPhoneBreak:px-10 py-4 text-xl w-full max-w-[1440px]">
+            <NavLink>
+                <Logo />
+            </NavLink>
+            <Link href="https://moth-app-prod-qoyzv.ondigitalocean.app/" target="_blank" rel="noopener noreferrer">
+                <KoshaButton type="secondary">
+                    Join Now
+                </KoshaButton>
+            </Link>
+        </div>
     </nav>
 }
