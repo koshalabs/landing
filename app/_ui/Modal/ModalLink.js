229d6c02a4d881ede08a3b8f740734ba50d3c645
g-smartynov <s.martynov@geniusee.com>
2024-12-09 16:16:04 +0200

    fix router scroll

--- app/_ui/Modal/ModalLink.js
+++ app/_ui/Modal/ModalLink.js
@@ -1,7 +1,8 @@
 import Link from "next/link";
+import { enableScroll } from "../_actions/helpers";
 
 export default function ModalLink({children, href}) {
-    return <Link href={href || "#"} className="font-medium underline">
+    return <Link href={href || "#"} className="font-medium underline" onClick={enableScroll}>
         {children}
     </Link>
 }
