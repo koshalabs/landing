5e879704648f24a071524cdd78ad19cd69a300d9
g-smartynov <s.martynov@geniusee.com>
2024-12-09 15:56:23 +0200

    remove target _blank on inner links

--- app/_ui/Modal/ModalLink.js
+++ app/_ui/Modal/ModalLink.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function ModalLink({children, href}) {
-    return <Link href={href || "#"} className="font-medium underline" target="_blank">
+    return <Link href={href || "#"} className="font-medium underline">
         {children}
     </Link>
 }
--- app/_ui/SectionElements/KoshaLink.js
+++ app/_ui/SectionElements/KoshaLink.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 export default function KoshaLink({children, href = "#"}) {
-    return <Link href={href} className="text-koshaLink underline text-[16px]" target="_blank">
+    return <Link href={href} className="text-koshaLink underline text-[16px]">
         {children}
     </Link>
 }
