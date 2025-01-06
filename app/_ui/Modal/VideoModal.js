24d29ca3496b58c676db819535252bb82e96fdbf
g-smartynov <s.martynov@geniusee.com>
2025-01-06 19:03:57 +0200

    fix the mobile video overflow

--- app/_ui/Modal/VideoModal.js
+++ app/_ui/Modal/VideoModal.js
@@ -1,5 +1,5 @@
 export default function VideoModal({handleCloseModal}) {
-    return <div className="bg-white overflow-y-auto w-[calc(100svw-32px)] footerPhoneBreak:w-[calc(100svw-80px)] max-w-[1360px] modalShadow text-koshaBlack relative modalWindow aspect-video max-h-[calc(100dvh-100px)]">
+    return <div className="bg-white overflow-y-auto w-[calc(100svw-32px)] footerPhoneBreak:w-[calc(100svw-80px)] max-w-[1360px] modalShadow text-koshaBlack relative modalWindow aspect-video max-h-[calc(100dvh-100px)] overflow-x-hidden">
         <iframe className="inset-0 w-[calc(100%+13px)] h-full border-0 absolute right-0 bottom-0" src="https://www.youtube.com/embed/Fez4Dwp501g?si=rkdcaxDF1yeQXAQ-&autoplay=1" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
     </div>;
 } 
